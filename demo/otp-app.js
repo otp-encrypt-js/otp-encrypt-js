824c873099524cf1e6fba199a3008cf2780641ff
eklem <espen.klem@gmail.com>
2025-03-10 22:27:16 +0100

    Demo - plaincoding Got to plaincoding message

--- demo/otp-app.js
+++ demo/otp-app.js
@@ -1,6 +1,5 @@
 import { watch, reactive, html } from 'https://esm.sh/@arrow-js/core'
-import { createOneTimePad, codebook, checkLength } from './otp-encrypt-js.browser.esm.js'
-// , textToPlaincode, plaincodeToText, encryptPlaincode, decryptEncryptedMsg, eng, nob }
+import { createOneTimePad, codebook, checkLength, textToPlaincode, plaincodeToText, encryptPlaincode, decryptEncryptedMsg, eng } from './otp-encrypt-js.browser.esm.js'
 
 const otp = createOneTimePad(128)
 const lengthValid = checkLength('some string', otp)
@@ -9,6 +8,7 @@ console.log(codebook[0].id + ' ' + codebook[0].emoji)
 console.log(otp)
 console.log(lengthValid)
 
+// ### Data
 const data = reactive({
   otp: '',
   otpLength: 64,
@@ -19,10 +19,6 @@ const data = reactive({
   decryptedMessage: ''
 })
 
-function generateOtp () {
-  data.otp = createOneTimePad(64)
-}
-
 // ## template
 const appElement = document.getElementById('app')
 
@@ -30,16 +26,16 @@ const template = html`
   <div id="otp">
     <h2>1. OTP (one-time pad)</h2>
     <textarea placeholder="paste or generate one-time pad" value="${() => data.otp}"></textarea><br />
-    <input type="number" id="otplength" value="128" /><button @click="${generateOtp}">Generate one-time pad</button>
+    <input type="number" id="otplength" value="64" @change="${updateOtpLength}" /><button @click="${generateOtp}">Generate one-time pad</button>
   </div>
   <div id="message">
     <h2>2. Message</h2>
-    <textarea placeholder="message"></textarea>
+    <textarea id="messageText" placeholder="message" @input="${getMessage}"></textarea>
     <div>characters left and too long: true/false</div>
   </div>
   <div id="plaincodedMessage">
     <h2>3. Plaincoded message</h2>
-    <textarea placeholder="plaincode"></textarea>
+    <textarea placeholder="plaincode" value="${() => data.plaincode}"></textarea>
   </div>
   <div id="encryptedMessage">
     <h2>4. Encrypted message</h2>
@@ -55,3 +51,35 @@ const template = html`
   </div>
 `
 template(appElement)
+
+// ### Functions
+
+function generateOtp () {
+  data.otp = createOneTimePad(data.otpLength)
+}
+
+function updateOtpLength () {
+  data.otpLength = document.getElementById('otplength').valueAsNumber
+  console.log(data.otpLength)
+}
+
+function getMessage () {
+  if (document.getElementById('messageText').value !== null) {
+    data.message = document.getElementById('messageText').value
+    console.log(data.message)
+  }
+}
+
+// ### Watchers
+
+data.$on('message', (value) => {
+
+})
+
+function plaincodeMessage () {
+  if (data.message) {
+    data.plaincode = textToPlaincode(data.message, eng, codebook)
+  }
+}
+
+watch(plaincodeMessage)
